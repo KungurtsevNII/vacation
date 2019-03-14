d132da6ede0394828b8752b484a6aa426ae3cb96
KungurtsevNII <KungurtsevNI@gmail.com>
2019-03-14 11:50:22 +0400

    главная страница с опсианием

--- views/site/index.php
+++ views/site/index.php
@@ -5,49 +5,29 @@
 $this->title = 'Отпуска';
 ?>
 <div class="site-index">
-
-    <div class="jumbotron">
-        <h1>Congratulations!</h1>
-
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
-
-    <div class="body-content">
-
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-            </div>
-        </div>
-
-    </div>
+    <p align="center">
+        <a href="http://a0252368.xsph.xsph.ru/" target="_blank"><h1 align="center">Работа системы</h1></a>
+    </p>
+    <h2>Описание системы</h2>
+    <p>В системе есть две роли: сотрудник и руководитель. </p>
+    <p>Рядовому сотруднику доступен следющий функционал: </p>
+    <ul>
+        <li>Забронировать отпуск</li>
+        <li>Посмотреть какие даты отпусков у других сотрудников</li>
+        <li>Скорректировать интервалы своих отпусков</li>
+    </ul>
+    <p>Руководителю доступен следующий функционал:</p>
+    <ul>
+        <li>Полномочия рядового сотрудника</li>
+        <li>Поставить признак, что данные по отпуску конкретного сотрудника зафиксированы, т.е. согласовать отпуск СВОЕГО подчиненного</li>
+    </ul>
+    <p>После того, как руководитель согласовал отпуск подчиненного, сотрудник не может скорректировать данный отпуск.</p>
+    <p style="color:red">Cотрудник не может корректировать не свой отпуск, а руководитель может согласовывать отпуска только своих подчиненных.</p>
+    <h3>Данные для входа:</h3>
+    <p>У всех пользователей пароль - ******. Табельный номер можно взять из таблица `employees`. Дамп базы данных можно найти в корне проекта - `vacation.sql`.</p>
+    <h3>Технологии:</h3>
+    <ul>
+        <li>Yii2</li>
+        <li>MySQL</li>
+    </ul>
 </div>
